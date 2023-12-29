f6a6bf780313fd9f54753e6d9c109c638934ba24
Christopher <114704469+Blade-24@users.noreply.github.com>
2023-12-29 10:48:02 -0600

    input range update

--- js/themes.js
+++ js/themes.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const themeButton = document.getElementById("change-theme");
 
 const body = document.querySelector(".body");
@@ -12,7 +14,9 @@ const resetKey = document.getElementById("reset-button");
 const equalKeyContainer = document.querySelector(".main-container__keys__button-container-equal");
 const equalKey = document.getElementById("equal-button");
 
+// *default style
 body.classList.add("body-1");
+themeButton.classList.add("input-theme-1");
 resultSection.classList.add("result-1");
 keys.classList.add("keys-1");
 
@@ -28,8 +32,10 @@ resetKey.classList.add("reset-1");
 equalKeyContainer.classList.add("theme-equal-1");
 equalKey.classList.add("equal-1");
 
+// *Remove themes
 const removeClasses = () => {
   body.classList.remove("body-1", "body-2", "body-3");
+  themeButton.classList.remove("input-theme-1", "input-theme-2", "input-theme-3");
   resultSection.classList.remove("result-1", "result-2", "result-3");
   keys.classList.remove("keys-1", "keys-2", "keys-3");
 
@@ -46,64 +52,41 @@ const removeClasses = () => {
   equalKey.classList.remove("equal-1", "equal-2", "equal-3");
 }
 
+// *Add themes
+const addTheme = (value) => {
+  body.classList.add(`body-${value}`);
+  themeButton.classList.add(`input-theme-${value}`);
+  resultSection.classList.add(`result-${value}`);
+  keys.classList.add(`keys-${value}`);
+
+  numbersKeysContainer.forEach(keyContainer => keyContainer.classList.add(`theme-${value}`));
+  numbersKeys.forEach(number => number.classList.add(`button-normal-theme-${value}`));
+
+  removeKeyContainer.classList.add(`theme-delete-${value}`);
+  removeKey.classList.add(`delete-${value}`);
+
+  resetKeyContainer.classList.add(`theme-reset-${value}`);
+  resetKey.classList.add(`reset-${value}`);
+
+  equalKeyContainer.classList.add(`theme-equal-${value}`);
+  equalKey.classList.add(`equal-${value}`);
+}
+
+// *Change theme
 themeButton.addEventListener("input", () => {
   const themeValue = themeButton.value;
 
   removeClasses();
 
   if (themeValue === "0") {
-
-    body.classList.add("body-1");
-    resultSection.classList.add("result-1");
-    keys.classList.add("keys-1");
-
-    numbersKeysContainer.forEach(keyContainer => keyContainer.classList.add("theme-1"));
-    numbersKeys.forEach(number => number.classList.add("button-normal-theme-1"));
-
-    removeKeyContainer.classList.add("theme-delete-1");
-    removeKey.classList.add("delete-1");
-
-    resetKeyContainer.classList.add("theme-reset-1");
-    resetKey.classList.add("reset-1");
-
-    equalKeyContainer.classList.add("theme-equal-1");
-    equalKey.classList.add("equal-1");
-
+    addTheme(1);
   }
 
   if (themeValue === "1") {
-    body.classList.add("body-2");
-    resultSection.classList.add("result-2");
-    keys.classList.add("keys-2");
-
-    numbersKeysContainer.forEach(keyContainer => keyContainer.classList.add("theme-2"));
-    numbersKeys.forEach(number => number.classList.add("button-normal-theme-2"));
-
-    removeKeyContainer.classList.add("theme-delete-2");
-    removeKey.classList.add("delete-2");
-
-    resetKeyContainer.classList.add("theme-reset-2");
-    resetKey.classList.add("reset-2");
-
-    equalKeyContainer.classList.add("theme-equal-2");
-    equalKey.classList.add("equal-2");
+    addTheme(2);
   }
 
   if (themeValue === "2") {
-    body.classList.add("body-3");
-    resultSection.classList.add("result-3");
-    keys.classList.add("keys-3");
-
-    numbersKeysContainer.forEach(keyContainer => keyContainer.classList.add("theme-3"));
-    numbersKeys.forEach(number => number.classList.add("button-normal-theme-3"));
-
-    removeKeyContainer.classList.add("theme-delete-3");
-    removeKey.classList.add("delete-3");
-
-    resetKeyContainer.classList.add("theme-reset-3");
-    resetKey.classList.add("reset-3");
-
-    equalKeyContainer.classList.add("theme-equal-3");
-    equalKey.classList.add("equal-3");
+    addTheme(3);
   }
 })
